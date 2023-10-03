4ccdef400dc75a71ee3a265e5165371d64a23bc4
stase-dima <d.stasenko@mirtekgroup.ru>
2023-10-03 10:00:44 +0300

    Translating of comments and delete debug arrays

--- Tauriga test/messages_parser.c
+++ Tauriga test/messages_parser.c
@@ -9,19 +9,6 @@
 #define MIN(x,y) (x < y ? x:y)
 #define MAX(x,y) (x > y ? x:y)
 
-typedef enum {
-    UNKNOWN = 0,
-    MESSAGE = 1,
-    MASK = 2
-} input_type_t;
-
-typedef struct {
-    list_t *another_message;
-    list_t *another_mask;
-    size_t pair_number;
-    size_t pair_counter;
-} counters_t;
-
 typedef struct {
     uint8_t type;
     size_t length;
@@ -30,9 +17,6 @@ typedef struct {
     uint32_t crc32;
 } message_t;
 
-typedef struct {
-    char input_buf [258];
-} buffers_t;
 
 uint32_t mask_parsing (d_array_t *item, size_t position);
 uint8_t pack_byte (d_array_t *item);
@@ -40,7 +24,8 @@ message_t *message_parsing (d_array_t *item, size_t position);
 uint32_t mask_parsing (d_array_t *item, size_t position);
 uint32_t mask_parsing (d_array_t *item, size_t position);
 
-// Преобразует ascii символ в половину байта
+
+// Convert symbol from input to uint-number
 uint8_t ascii_to_half_byte(const uint8_t symbol_code)
 {
     if (symbol_code >= 0x41 && symbol_code <= 0x46)
@@ -53,7 +38,8 @@ uint8_t ascii_to_half_byte(const uint8_t symbol_code)
     }
 }
 
-// Преобразует два идущих последовательно символа ввода в байт
+
+// Packing two following symbols to one byte
 uint8_t pack_byte (d_array_t *item)
 {
     uint8_t result_byte = 0;
@@ -62,33 +48,33 @@ uint8_t pack_byte (d_array_t *item)
     return result_byte;
 }
 
-// Парсинг сообщения
-message_t *message_parsing (d_array_t *item, size_t position){
+
+// Parsing of input array to message struct
+message_t *message_parsing (d_array_t *item, size_t position)
+{
     message_t *current_message = malloc(sizeof(message_t));
 
-    uint8_t array[20] = {};
-    // memcpy(array, item->array, 24);
-
+    // Set position of next element
     set_pointer_by_position(item, position);
 
-    // Забираем из сообщения тип
+    // Get the type
     current_message->type = pack_byte(item);
-    // Забираем из сообщения длину поля данных
+    // Get the length
     current_message->length = pack_byte(item);
 
-    // Вычисляем итоговую длину массива данных, если исходная не кратная 4
+    // Count length, myltiplyed by 4
     current_message->final_length = current_message->length;
     if (current_message->final_length % 4)
     {
         current_message->final_length += 4 - (current_message->final_length % 4);
     }
 
-    // Выделяем память под данные
+    // Allocation memory for message and creating floating pointer data_pointer to movement of data bytes
     uint32_t *data_pointer = malloc(sizeof(int*) * current_message->length);
     uint32_t *data_start = data_pointer;
     uint32_t tmp;
     
-    // Сохраняем сначала реальные данные, а потом оставляем нулями остаток если он есть
+    // Getting data from array
     for (int i = 0; i < (current_message->final_length / 4); i++)
     {
         *data_pointer = 0;
@@ -99,11 +85,11 @@ message_t *message_parsing (d_array_t *item, size_t position){
         }
         data_pointer++;
     }
-    current_message->data = (uint8_t*)data_start;
-    memcpy(array, current_message->data, 16);
-    
 
-    // Сохраняем CRC32
+    // Saving data pointer to struct
+    current_message->data = (uint8_t*)data_start;    
+
+    // Save CRC32
     current_message->crc32 = 0;
     for (int i = 0; i < 4; i++)
     {
@@ -114,19 +100,18 @@ message_t *message_parsing (d_array_t *item, size_t position){
 }
 
 
-// Парсинг маски из ввода
-uint32_t mask_parsing (d_array_t *item, size_t position){
+// Parsing of mask and saving it to uint32
+uint32_t mask_parsing (d_array_t *item, size_t position)
+{
+    // Floating pointer to position in array
     set_pointer_by_position(item, position);
+
     uint32_t result_mask = 0;
     uint8_t byte = 0;
 
-    uint8_t array[2] = {};
-    memcpy(array, item->array_pointer, 2);
-
-    // Сохраняем маску
+    // Saving mask
     for (int i = 0; i < 4; i++)
     {
-        memcpy(array, item->array_pointer, 2);
         byte = pack_byte(item);
         result_mask |= (byte << (24 - i * 8));
     }
@@ -135,22 +120,23 @@ uint32_t mask_parsing (d_array_t *item, size_t position){
 }
 
 
-// Наложение одной маски на одно сообщение с генерацией нового сообщения
+// Overlay mask on data array and creating new changed message struct
 message_t *mask_overlay (message_t *recieved_message, uint32_t mask)
 {
-    // uint32_t array[20] = {};
-    // memcpy(array, recieved_message->data, 12);
+    // Memory for new message
     message_t *new_message = malloc(sizeof(message_t));
-    // Вывод данных в динамический массив для редактирования
+    // Allocation of memory field for edit data
     uint32_t *process_data = malloc(sizeof(uint32_t) * recieved_message->final_length);
     uint32_t *process_data_pointer = process_data;
+    // Save old data field
     memcpy(process_data, recieved_message->data, recieved_message->final_length);
-    // Инициализируем поля нового сообщения и выделяем под него память
+    // Allocated memory for eddited message struct and init fields beside of data
     new_message->type = recieved_message->type;
     new_message->final_length = recieved_message->final_length;
     new_message->length = recieved_message->length;
     new_message->data = malloc(sizeof(uint32_t) * recieved_message->final_length);
 
+    // Overlaying mask to all data fields
     uint32_t temp;
     for (int i = 0; i < (recieved_message->final_length); i++)
     {
@@ -159,6 +145,7 @@ message_t *mask_overlay (message_t *recieved_message, uint32_t mask)
         process_data_pointer++;
     }
 
+    // Calc CRC32 of message without crc
     uint32_t CRC32 = 0xFFFFFFFFull;
     CRC32 = crc32c(CRC32, new_message->type);
     CRC32 = crc32c(CRC32, new_message->final_length);
@@ -173,6 +160,7 @@ message_t *mask_overlay (message_t *recieved_message, uint32_t mask)
         }
     }
 
+    // Saving crc32 to struct
     new_message->crc32 = CRC32;
 
     new_message->data = (uint8_t*)process_data;
@@ -180,7 +168,8 @@ message_t *mask_overlay (message_t *recieved_message, uint32_t mask)
     return new_message;
 }
 
-// Вывод совокупности обоих сообщений в файл
+
+// Output of old and new verions of a message to a file
 void print_message_to_file (message_t *old_message, message_t *new_message, FILE *target_file)
 {
     uint32_t *data_pointer = NULL;
@@ -189,6 +178,7 @@ void print_message_to_file (message_t *old_message, message_t *new_message, FILE
     fprintf(target_file, "Initial message length: %d\n", old_message->length);
     fprintf(target_file, "Initial message data bytes: ");
 
+    // Print tetrads of initial messate
     data_pointer = (uint32_t*)old_message->data;
     for (int i = 0; i < (old_message->length / 4); i++)
     {
@@ -196,8 +186,11 @@ void print_message_to_file (message_t *old_message, message_t *new_message, FILE
         data_pointer++;
     }
 
+    // Print incomplete tetrad one byte at time
     int remaining_bytes = old_message->length % 4;
+    // Needed turn around bytes inside the uint32
     uint8_t remaining_part [4] = {((uint8_t*)data_pointer)[3], ((uint8_t*)data_pointer)[2], ((uint8_t*)data_pointer)[1], ((uint8_t*)data_pointer)[0]};
+    // Print by byte
     if (remaining_bytes > 0) {
         uint8_t *byte_pointer = (uint8_t*)data_pointer;
         for (int i = 0; i < remaining_bytes; i++) {
@@ -223,59 +216,62 @@ void print_message_to_file (message_t *old_message, message_t *new_message, FILE
     fprintf(target_file, "0x%08x\n\n", new_message->crc32);
 }
 
+
+
 int main (void)
 {
-    // Открыть файл ввода
+    // Opening of file to read
     FILE *input_file;
     input_file = fopen("input.txt", "r");
-    // Открыть файл вывода на запись
+    // Opening of file to write
     FILE *output_file;
     output_file = fopen("output.txt", "a");
 
-    // Хранилище обрабатываемого символа
+    // Storage of each symbol from an input
     char symbol;
     
-    // Собираем весь ввод в динамический массив
+    // Packing all input to a dynamic array
     d_array_t *d_array_input = init_array(10);
     do {
         symbol = fgetc(input_file);
         add_element(d_array_input, symbol);
     } while (symbol != EOF);
     
-    counters_t counters = {NULL, NULL, 0, 0};
+    // The number of pairs is equal to the minimum number of messages and masks.
+    size_t pair_number = 0;
+    pair_number = MIN(count_anchors(d_array_input, "mess="), count_anchors(d_array_input, "mask="));
 
-    counters.pair_number = MIN(count_anchors(d_array_input, "mess="), count_anchors(d_array_input, "mask="));
+    // Storage for mask
     uint32_t mask = 0;
 
+    // Positions of nexst message and mask to easy movement without them in the array
     size_t array_mess_position = 0;
     size_t array_mask_position = 0;
-    message_t *new_message = NULL;
+    // Structures for storaging the next message in initial and modified version
     message_t *old_mess = NULL;
     message_t *new_mess = NULL;
-    uint32_t array[20] = {};
-    // Проходим по каждой паре
-    for (int i = 0; i < counters.pair_number; i++)
+    // Movement for each pair of message and mask
+    for (int i = 0; i < pair_number; i++)
     {
-        // Для каждого анкора ищем очередную позицию
+        // Find out the position of the next message
         array_mess_position = get_anchor_position(d_array_input, "mess=", array_mess_position);
-        // По позиции парсим сообщение и сохраняем его в выделенную область
+        // For the each anchor "mess=" performing parsing
         old_mess = message_parsing(d_array_input, array_mess_position);
-        memcpy(array, old_mess->data, 20);
-        // Находим ближайшую маску
+        // Find out the position of the next mask
         array_mask_position = get_anchor_position(d_array_input, "mask=", array_mask_position);
-        // Парсим маску
+        // For the each anchor "mask=" performing parsing
         mask = mask_parsing(d_array_input, array_mask_position);
-        // Накладываем маску на исходное сообщение и формируем новое
+        // Overlaying mask to message data and recalculation of a CRC-32
         new_mess = mask_overlay(old_mess, mask);
-        // Выводим в файл результаты
+        // Output to the file
         print_message_to_file(old_mess, new_mess, output_file);
-        // Освобождаем память
+        // Freeing of memory
         free(old_mess->data);
         free(old_mess);
         free(new_mess->data);
         free(new_mess);
     }
 
-    // Освобождаем память
+    // Freeing of memory
     free(d_array_input);
 }
